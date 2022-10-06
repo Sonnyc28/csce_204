1b1df6abc3d6d3d68779e5b37c8c57fd0fbfce0a
Sonnyc28 <89541665+Sonnyc28@users.noreply.github.com>
2022-10-06 12:56:09 -0400

    Delete CSCE204/CSCE204 directory

--- CSCE204/CSCE204/exercises/sep13/while1.py
+++
@@ -1,7 +0,0 @@
-an = input("Do you want me to say hello (Y)es or (N)o?").lower().strip()
-
-while an == "y":
-    print("Hello")
-    an = input("Do you want to say hello again?(Y)es or (N)o?").lower().strip()
-
-print("Bye for now")
